3d8ff6e5656d93aa917955a641ffb4deb2c32e25
박준영 <wnsdud6969@naver.com>
2024-06-18 21:44:06 +0900

    올바른괄호_LV2

---
+++ 올바른괄호_LV2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+       Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                } else if (stack.peek() == '(') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
